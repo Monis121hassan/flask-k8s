ade5ef1f9a1199d89685b4ab19bf697af48792e7
monis121hassan <monis121hassan@gmail.com>
2025-11-08 19:49:54 +0530

    Update app.py with new port listening

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask running on Kubernetes via ArgoCD!"
+    return "Hello from Flask running on Kubernetes via ArgoCD! Am i listening to the latest port?"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
